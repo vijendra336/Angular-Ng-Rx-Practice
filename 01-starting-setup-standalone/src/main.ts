6ae963c3ec2e66a972d89cf4c653b6defd5baf27
vijendra336 <39852345+vijendra336@users.noreply.github.com>
2023-11-09 21:02:35 +0530

    S26C372 Adding First Reducer counterReducer & Store Setup.

--- 01-starting-setup-standalone/src/main.ts
+++ 01-starting-setup-standalone/src/main.ts
@@ -2,7 +2,11 @@ import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
 import { provideStore } from '@ngrx/store';
+import { counterReducer } from './app/store/counter.reducer';
 
 bootstrapApplication(AppComponent, {
-    providers: [provideStore()]
+    providers: [provideStore({
+        counter: counterReducer
+        // just like counterReducer you can add more reducer here key: Value pair 
+    })]
 });
