809826719a81f1e24d6686a6d8f4b2b0e5bb5db8
vijendra336 <39852345+vijendra336@users.noreply.github.com>
2023-11-09 21:54:07 +0530

    S26C374 Reading data from store using store.select(counter).

--- 01-starting-setup/src/app/counter-output/counter-output.component.ts
+++ 01-starting-setup/src/app/counter-output/counter-output.component.ts
@@ -1,28 +1,21 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Subscription } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 
-import { CounterService } from '../counter.service';
+import { Store } from '@ngrx/store';
 
 @Component({
   selector: 'app-counter-output',
   templateUrl: './counter-output.component.html',
   styleUrls: ['./counter-output.component.css'],
 })
-export class CounterOutputComponent implements OnInit, OnDestroy {
-  counter = 0;
-  counterServiceSub?: Subscription;
+export class CounterOutputComponent  {
 
-  constructor(private counterService: CounterService) {}
+  count$ : Observable<number>;
 
-  ngOnInit(): void {
-    this.counterServiceSub = this.counterService.counterChanged.subscribe(
-      (newVal) => (this.counter = newVal)
-    );
+  constructor(private store: Store<{counter:number}>) {
+    this.count$ = store.select('counter')
   }
 
-  ngOnDestroy(): void {
-    if (this.counterServiceSub) {
-      this.counterServiceSub.unsubscribe();
-    }
-  }
+
+
 }
