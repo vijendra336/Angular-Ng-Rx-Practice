dcb5543d201efd6122105c94a483218d5776a6e0
vijendra336 <39852345+vijendra336@users.noreply.github.com>
2023-11-09 20:33:57 +0530

    S26C371 Insatlling NgRx changes in both type of project.

--- 01-starting-setup-standalone/src/main.ts
+++ 01-starting-setup-standalone/src/main.ts
@@ -1,5 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
+import { provideStore } from '@ngrx/store';
 
-bootstrapApplication(AppComponent);
+bootstrapApplication(AppComponent, {
+    providers: [provideStore()]
+});
