2f7ed98b35016d074930445c29b816e9d26bb8f2
vijendra336 <39852345+vijendra336@users.noreply.github.com>
2023-11-10 09:32:24 +0530

    S26C375 Created increment Action and add it in Reducer in non standalone component.

--- 01-starting-setup/src/app/store/counter.reducer.ts
+++ 01-starting-setup/src/app/store/counter.reducer.ts
@@ -1,13 +1,15 @@
-import { createReducer } from "@ngrx/store";
+import { createReducer, on } from "@ngrx/store";
+import { increment } from "./counter.action";
 
 const initialState = 0;
 
 //1st way 
-// export const counterReducer = createReducer(
-//     initialState
-// );
+export const counterReducer = createReducer(
+    initialState,
+    on(increment, (state) => state + 1 )
+);
 
 // 2nd  way to create Reducer 
-export function counterReducer(state = initialState){
-    return state;
-}
+// export function counterReducer(state = initialState){
+//     return state;
+// }
