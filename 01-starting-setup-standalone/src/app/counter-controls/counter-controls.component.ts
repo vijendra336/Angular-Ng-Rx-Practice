635494288d1f0742223c1f9596335c8f7c03059e
vijendra336 <39852345+vijendra336@users.noreply.github.com>
2023-11-10 09:52:34 +0530

    S26C376  Dispatching Action ( increment ).

--- 01-starting-setup-standalone/src/app/counter-controls/counter-controls.component.ts
+++ 01-starting-setup-standalone/src/app/counter-controls/counter-controls.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
-import { CounterService } from '../counter.service';
+import { Store } from '@ngrx/store';
+import { increment } from '../store/counter.action';
 
 @Component({
   selector: 'app-counter-controls',
@@ -9,13 +10,13 @@ import { CounterService } from '../counter.service';
   standalone: true,
 })
 export class CounterControlsComponent {
-  constructor(private counterService: CounterService) {}
+  constructor(private store:Store) {}
 
   increment() {
-    this.counterService.increment();
+    this.store.dispatch(increment())
   }
 
   decrement() {
-    this.counterService.decrement();
+  
   }
 }
