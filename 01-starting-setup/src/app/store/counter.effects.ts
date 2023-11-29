b7788c3b7c0ee2fbf07b33e20eff8fb1d654ab53
vijendra336 <39852345+vijendra336@users.noreply.github.com>
2023-11-29 16:26:15 +0530

    S26C385 Register effect and make injectable.

--- 01-starting-setup/src/app/store/counter.effects.ts
+++ 01-starting-setup/src/app/store/counter.effects.ts
@@ -1,18 +1,21 @@
-import { Actions, createEffect, ofType } from "@ngrx/effects";
-import { decrement, increment } from "./counter.action";
-import { tap } from "rxjs";
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { decrement, increment } from './counter.action';
+import { tap } from 'rxjs';
+import { Injectable } from '@angular/core';
 
-export class CounterEffects{
-    
-    constructor(private actions$ : Actions){
-    }
+@Injectable()
+export class CounterEffects {
+  constructor(private actions$: Actions) {}
 
-    saveCount= createEffect(()=> this.actions$.pipe(
+  saveCount = createEffect(
+    () =>
+      this.actions$.pipe(
         ofType(increment, decrement),
         tap((action) => {
-            console.log(action);
-            localStorage.setItem('count',action.value.toString());
+          console.log(action);
+          localStorage.setItem('count', action.value.toString());
         })
-    ) );
-
+      ),
+    { dispatch: false } // means this effect does not dispatch a new action
+  );
 }
