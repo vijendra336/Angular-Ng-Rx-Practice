338522ea15318b9f78c66b481ec26cac403796f6
vijendra336 <39852345+vijendra336@users.noreply.github.com>
2023-11-29 17:11:13 +0530

    S26C387 Use store data in Effects.

--- 01-starting-setup-standalone/src/app/store/counter.effect.ts
+++ 01-starting-setup-standalone/src/app/store/counter.effect.ts
@@ -1,19 +1,22 @@
 import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { decrement, increment } from './counter.action';
-import { tap } from 'rxjs';
+import { tap, withLatestFrom } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { selectCount } from './counter.selector';
 
 @Injectable()
 export class CounterEffects {
-  constructor(private actions$: Actions) {}
+  constructor(private actions$: Actions, private store:Store<{counter:number}>) {}
 
   saveCount = createEffect(
     () =>
       this.actions$.pipe(
         ofType(increment, decrement),
-        tap((action) => {
+        withLatestFrom(this.store.select(selectCount)),
+        tap(([action, counter]) => {
           console.log(action);
-          localStorage.setItem('count', action.value.toString());
+          localStorage.setItem('count', counter.toString());
         })
       ),
     { dispatch: false }
--- 01-starting-setup/src/app/store/counter.effects.ts
+++ 01-starting-setup/src/app/store/counter.effects.ts
@@ -1,19 +1,23 @@
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { decrement, increment } from './counter.action';
-import { tap } from 'rxjs';
+import { tap, withLatestFrom } from 'rxjs';
 import { Injectable } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { selectCount } from './counter.selector';
 
 @Injectable()
 export class CounterEffects {
-  constructor(private actions$: Actions) {}
+  constructor(private actions$: Actions, private store: Store<{counter:number}>) {}
 
   saveCount = createEffect(
     () =>
       this.actions$.pipe(
         ofType(increment, decrement),
-        tap((action) => {
+        // withLatestFrom(this.store.select('counter')) // counter selector 
+        withLatestFrom(this.store.select(selectCount)), // selectCount from counter.selector.ts
+        tap(([action, counter]) => {
           console.log(action);
-          localStorage.setItem('count', action.value.toString());
+          localStorage.setItem('count', counter.toString());
         })
       ),
     { dispatch: false } // means this effect does not dispatch a new action
