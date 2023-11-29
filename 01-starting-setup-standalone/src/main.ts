5d0aebc99d2f111488b2ac86114728d1dad2da4f
vijendra336 <39852345+vijendra336@users.noreply.github.com>
2023-11-29 14:38:42 +0530

    S26C383 install effects ng add @ngrx/effects.

--- 01-starting-setup-standalone/src/main.ts
+++ 01-starting-setup-standalone/src/main.ts
@@ -3,10 +3,11 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideStore } from '@ngrx/store';
 import { counterReducer } from './app/store/counter.reducer';
+import { provideEffects } from '@ngrx/effects';
 
 bootstrapApplication(AppComponent, {
     providers: [provideStore({
         counter: counterReducer
         // just like counterReducer you can add more reducer here key: Value pair 
-    })]
+    }), provideEffects()]
 });
