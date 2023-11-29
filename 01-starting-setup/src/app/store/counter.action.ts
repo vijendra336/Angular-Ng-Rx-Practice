a07985e7b7f35921924589180230a7ebd9997477
vijendra336 <39852345+vijendra336@users.noreply.github.com>
2023-11-29 19:26:18 +0530

    S26C388  Adding a second Effects ( read data from localstorage and save into store ) init and set action.

--- 01-starting-setup/src/app/store/counter.action.ts
+++ 01-starting-setup/src/app/store/counter.action.ts
@@ -1,24 +1,20 @@
-import { Action, createAction, props } from "@ngrx/store";
+import { Action, createAction, props } from '@ngrx/store';
 
-//C388 load data from local storage 
-export const init =createAction(
-    '[Counter] Init'
-    // I don't need any extra data attached to the action.
-)
+//C388 load data from local storage
+export const init = createAction(
+  '[Counter] Init'
+  // I don't need any extra data attached to the action.
+);
 
-export const set =createAction(
-    '[Counter] Set',
-    props<{value:number}>()
-)
+export const set = createAction('[Counter] Set', props<{ value: number }>());
 
 // 1st way using createAction
 export const increment = createAction(
-    '[Counter] Increment', 
-    props<{value: number}>()
+  '[Counter] Increment',
+  props<{ value: number }>()
 );
 
-
-//2nd way creating action without createAction 
+//2nd way creating action without createAction
 
 // export class IncrementAction implements Action{
 //     readonly type=  '[Counter] Increment';
@@ -32,6 +28,6 @@ export const increment = createAction(
 // export type CounterActions = IncrementAction;
 
 export const decrement = createAction(
-    '[Counter] Decrement',
-    props<{value:number}>()
-)
+  '[Counter] Decrement',
+  props<{ value: number }>()
+);
